98f124824ef7e104776018b4ed600e02c62a198c
ancagi <ancagiraldo@gmail.com>
2020-12-20 17:20:09 -0500

    Nuevos item en registro

--- src/components/Registro.vue
+++ src/components/Registro.vue
@@ -1,55 +1,72 @@
 <template>
-<b-container>
-  <b-form class="myform mt-5">
-    <b-form-group id="input-group-1" label-for="input-1">
-      <b-form-input
-        id="email"
-        v-model="form.email"
-        type="email"
-        placeholder="Correo electronico"
-        required
-      ></b-form-input>
-    </b-form-group>
-    <b-form-group id="input-group-2" label-for="input-2">
-      <b-form-input
-        id="password"
-        v-model="form.password"
-        type="password"
-        placeholder="Contraseña"
-        required
-      ></b-form-input>
-    </b-form-group>
-    <b-form-group id="input-group-3" label-for="input-3">
-      <b-form-input
-        id="name"
-        v-model="form.name"
-        type="text"
-        placeholder="Nombre del hotel"
-        required
-      ></b-form-input>
-    </b-form-group>
-    <b-form-group id="input-group-4" label-for="input-4">
-      <b-form-input
-        id="ubication"
-        v-model="form.ubication"
-        type="text"
-        placeholder="Ubicación del hotel"
-        required
-      ></b-form-input>
-    </b-form-group>
-    <b-form-group id="input-group-4" label-for="input-4">
-      <b-form-input
-        id="price"
-        type="tel"
-        v-model="form.price"
-        placeholder="Precio estándar habitación"
-        required
-      ></b-form-input>
-    </b-form-group>
-    <b-button v-on:click="submit" type="submit" variant="primary">Submit</b-button>
-  </b-form>
-</b-container>
-
+  <b-container>
+    <b-form class="myform mt-5">
+      <b-form-group id="input-group-1" label-for="input-1">
+        <b-form-input
+          id="email"
+          v-model="form.email"
+          type="email"
+          placeholder="Correo electronico"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-2" label-for="input-2">
+        <b-form-input
+          id="password"
+          v-model="form.password"
+          type="password"
+          placeholder="Contraseña"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-3" label-for="input-3">
+        <b-form-input
+          id="username"
+          v-model="form.name"
+          type="text"
+          placeholder="Nombre del hotel"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-4" label-for="input-4">
+        <b-form-input
+          id="ubication"
+          v-model="form.ubication"
+          type="text"
+          placeholder="Ubicación del hotel"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-4" label-for="input-4">
+        <b-form-input
+          id="price"
+          type="tel"
+          v-model="form.price"
+          placeholder="Precio estándar habitación"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-5" label-for="input-5">
+        <b-form-input
+          id="image"
+          type="tel"
+          v-model="form.image"
+          placeholder="Pag web del hotel"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-6" label-for="input-6">
+        <b-form-input
+          id="description"
+          type="text"
+          v-model="form.description"
+          placeholder="Precio estándar habitación"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-button v-on:click="submit" type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </b-container>
 </template>
 
 <script>
@@ -68,24 +85,24 @@ export default {
     };
   },
   methods: {
-    contrasena: function () {
-      var contrasena_1 = document.getElementById("contra1").value;
-      var contrasena_2 = document.getElementById("contra2").value;
-      if (contrasena_1 == contrasena_2) {
-        return true;
-      } else {
-        return false;
-      }
-    },
     submit: function () {
       axios
-        .post("https://g3m3-e9-hoteles-api.herokuapp.com/hotel/agregar", JSON.stringify(this.form))
+        .post(
+          "https://g3m3-e9-hoteles-api.herokuapp.com/hotel/agregar",
+          JSON.stringify(this.form)
+        )
         .then((result) => {
-          alert("Se ingreso");
+          var profile = true;
         })
         .catch((error) => {
-          alert("ERROR Servidor", error.response.data);
+          if (error.response.status == 400) {
+            alert("El hotel ya existe", error.response.data);
+            this.$router.push({ path: "/registro" });
+          }
         });
+      if (profile == true) {
+        this.$router.push({ name: "user_profile", params: { email: form.email } });
+      }
     },
   },
 };
